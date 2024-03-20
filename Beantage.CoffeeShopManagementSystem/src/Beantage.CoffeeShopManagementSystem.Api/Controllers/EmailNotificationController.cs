eedc8c8d71bfb9a6291d310021a38ed57f829d19
Rocky Rocamora <113041227+metamorphic-rock@users.noreply.github.com>
2024-03-21 03:16:09 +0800

    added new services

---
+++ Beantage.CoffeeShopManagementSystem/src/Beantage.CoffeeShopManagementSystem.Api/Controllers/EmailNotificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Beantage.CoffeeShopManagementSystem.Api.Controllers
+{
+	public class EmailNotificationController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Beantage.CoffeeShopManagementSystem/src/Beantage.CoffeeShopManagementSystem.Application/Services/EmailNotificationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beantage.CoffeeShopManagementSystem.Application.Services;
+
+public class EmailNotificationService
+{
+}
