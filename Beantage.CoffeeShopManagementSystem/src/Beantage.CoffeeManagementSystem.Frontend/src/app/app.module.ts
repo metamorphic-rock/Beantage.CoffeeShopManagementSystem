f2df29cca3b8d20548c593b41c8e9c3d1debd91c
Rocky Rocamora <113041227+metamorphic-rock@users.noreply.github.com>
2024-04-17 02:17:04 +0800

    added the new dependecies for popup modal

--- Beantage.CoffeeShopManagementSystem/src/Beantage.CoffeeManagementSystem.Frontend/src/app/app.module.ts
+++ Beantage.CoffeeShopManagementSystem/src/Beantage.CoffeeManagementSystem.Frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -59,6 +59,7 @@ const appRoutes : Routes = [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    NgbModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
